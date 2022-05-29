3581c731007c90ed2ea57156bc7ebdc4a246b28a
hjara <jeffer.diaz1894@gmail.com>
2022-05-29 02:12:39 -0500

    registry-eureka

--- seguridad-api/src/main/java/pe/code/migracion/seguridad/boot/SeguridadApplicationBoot.java
+++ seguridad-api/src/main/java/pe/code/migracion/seguridad/boot/SeguridadApplicationBoot.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @ConfigurationProperties
 @SpringBootApplication(scanBasePackages = { "pe.code.migracion" })
 @EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
+@EnableEurekaClient
 public class SeguridadApplicationBoot {
 
 	public static void main(String[] args) {
